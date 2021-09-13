a954948ec7f042917b319f68fd65279e5327df0a
Wouter <WouterVanGorp@users.noreply.github.com>
2021-09-13 22:04:38 +0200

    scroll  and explanation

--- src/components/PokemonDetail/PokemonDetail.tsx
+++ src/components/PokemonDetail/PokemonDetail.tsx
@@ -1,6 +1,7 @@
 import style from './../../App.module.css';
 import { PokemonDetail as Pokemon } from "./../../models";
 import { PokemonStats, PokemonPhoto } from 'components';
+import { useState } from 'react';
 
 interface Props {
   pokemon: Pokemon;
@@ -8,13 +9,20 @@ interface Props {
 }
 
 export function PokemonDetail({ pokemon, onSavePokemon }: Props) {
+
+  const [pokeState, setPokemon] = useState(pokemon);
+
+  // changes op de moves gaan we via setPokemon doen
+  // Op een save kunne we dan onze 'local' pokemon naar boven gooien
+
   return (
     <div className={style.flexRow}>
       <div>
-          <PokemonPhoto name={pokemon.name} img={pokemon.image} />
+          <PokemonPhoto name={pokeState.name} img={pokeState.image} />
+          <button onClick={() => onSavePokemon(pokeState)} >SAVE POKEMON</button>
       </div>
       <div>
-        <PokemonStats stats={pokemon.stats} />
+        <PokemonStats stats={pokeState.stats} />
       </div>
       <div></div>
     </div>
--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,6 +1,8 @@
 import { PokemonOverview as Pokemon } from "./../../models";
 import { PokemonItem } from "./../PokemonItem/PokemonItem";
 
+import style from './PokemonList.module.css';
+
 interface Props {
   pokemons: Pokemon[];
   onClick: (name: string) => void;
@@ -8,7 +10,7 @@ interface Props {
 
 export function PokemonList({ pokemons, onClick }: Props) {
   return (
-    <div>
+    <div className={style.root}>
       {pokemons.length === 0 ? "No pokemons found" : ""}
       {pokemons.map((p) => (
         <PokemonItem onClick={onClick} key={p.id} name={p.name} />
